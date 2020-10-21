efaaa71541ee1e4b65d143221f37be5582e5e839
김시영 <ssks1013@gmail.com>
2020-10-21 17:31:27 +0900

    cork board base

---
+++ Frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo-black .png"
+          transition="scale-transition"
+          width="180"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        target="_blank"
+        text
+      >
+        <v-icon color="black">mdi-login</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
