2d741b22c95a3ca78a63526b12b4c2abcffac76d
Jaewan <dksud67@naver.com>
2020-10-23 02:43:23 +0900

    채팅기능 추가

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -6,6 +6,17 @@ import vuetify from './plugins/vuetify';
 import VueDraggableResizable from 'vue-draggable-resizable'
 import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
+import Directives from '../plugin/directives'
+import VueMaterial from 'vue-material'
+
+
+import io from 'socket.io-client'; 
+const socket = io('http://localhost:3000');
+
+Vue.prototype.$socket = socket;
+Vue.use(Directives)
+Vue.use(VueMaterial)
+
 Vue.component('vue-draggable-resizable', VueDraggableResizable)
 
 Vue.config.productionTip = false
