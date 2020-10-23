e3ef6fe799e638479e9d3e817d227a04afbebcb9
JANG JONG HA <jangjongha.sw@gmail.com>
2020-10-23 11:05:58 +0900

    [Backend] environs applied to backend
    보안 강화를 위해 중요한 내용을 보관하는 environs 환경을 구축했습니다.
    인증키나 보안키와 같은 중요한 내용은 이곳에서 관리하시기 바랍니다.
    
    Signed-off-by: JANG JONG HA <jangjongha.sw@gmail.com>

--- Backend/vision_space/settings.py
+++ Backend/vision_space/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
+import os, environs
 from pathlib import Path
+
+# env_base = environs.Path(__file__)
+# print(env_base.parent.parent)
+# print(env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environs.Env()
+env.read_env(path=BASE_DIR.joinpath('./.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -159,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'developmaster1121'
-EMAIL_HOST_PASSWORD = 'hnxquoiytxcgkjpj'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # CORS Allow
 CORS_ORIGIN_ALLOW_ALL = True
