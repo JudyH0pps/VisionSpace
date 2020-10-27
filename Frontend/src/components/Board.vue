a555229803a922eaf2044c9afeffdcc7cb6801cc
Siyoung Kim <sskss1013@gmail.com>
2020-10-27 17:13:47 +0900

    노트 삭제기능

--- Frontend/src/components/Board.vue
+++ Frontend/src/components/Board.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="cork" style="height:100%; width: 100%;position: relative;">
     <BoardDrawer @add_note="add" />
-    <vue-draggable-resizable v-for="note in notes" :key="note.no" :w="220" :h="220" :x="note.x" :y="note.y" @dragging="onDrag" :resizable="false" :parent="true" :drag-handle="'.line'">
+    <vue-draggable-resizable v-for="(note,index) in notes" :key="note.no" :w="220" :h="220" :x="note.x" :y="note.y" @dragging="onDrag" :resizable="false" :parent="true" :drag-handle="'.line'">
       <svg class="line" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="40" height="40" viewBox="0 0 24 24"><path d="M16,12V4H17V2H7V4H8V12L6,14V16H11.2V22H12.8V16H18V14L16,12Z" /></svg>
       <div class="content" v-html="note.content">
       </div>
       <div style="position:absolute;left:5px;bottom:5px;">
-          <v-icon>mdi-trash-can-outline</v-icon>
+          <v-icon class="del_btn" @click="del_note(index)">mdi-trash-can-outline</v-icon>
       </div>
     </vue-draggable-resizable>
   </div>
@@ -75,6 +75,9 @@ export default {
         new_note.y = 0;
         new_note.content = '<p>' + text + '</p>'
         this.notes.push(new_note)
+    },
+    del_note(no) {
+        this.notes.splice(no,1);
     }
   },
   components: {
@@ -131,5 +134,12 @@ export default {
   align-items: center; */
   /* font-size: 2em; */
   font-size: 25px;
+}
+.del_btn{
+    color:#e9faab;
+    cursor: pointer;
+}
+.del_btn:hover{
+    color:#47493e;
 }
 </style>
