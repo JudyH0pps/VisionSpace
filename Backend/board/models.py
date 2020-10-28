d0d4d5de1678c69e81372c82c2a2682d79648d5b
JANG JONG HA <jangjongha.sw@gmail.com>
2020-10-28 17:06:54 +0900

    [Backend] Tab 및 Note 관련 API 개발 진행 중
    현재 Tab 관련해서 기본 적인 기능은 동작하나, 유저 Verification 기능은
    아직 완료되지 않음
    
    Signed-off-by: JANG JONG HA <jangjongha.sw@gmail.com>

--- Backend/board/models.py
+++ Backend/board/models.py
@@ -4,7 +4,8 @@ from django.conf import settings
 # Create your models here.
 class Board(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    user_list = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="boards", through="User_Board")  # 보드를 생성/관리하는 메인 관리자
+    user_list = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="user_boards", through="User_Board")  # 보드를 생성/관리하는 메인 관리자
+    max_tab_index = models.IntegerField(default=0)
 
 class User_Board(models.Model):
     user_pk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
@@ -12,28 +13,32 @@ class User_Board(models.Model):
     joined_date = models.DateTimeField(auto_now_add=True)
     is_admin = models.BooleanField()
 
-class Tab(models.Model):
-    board_pk = models.ForeignKey(Board, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-
 class Type(models.Model):
     name = models.CharField(max_length=20)
+
+class Tab(models.Model):
+    board_pk = models.ForeignKey(Board, on_delete=models.CASCADE)
+    tab_index = models.IntegerField(default=0)
+    name = models.CharField(max_length=50)
+    max_note_index = models.IntegerField(default=0)
 
 class Note(models.Model):
     user_pk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
     tab_pk = models.ForeignKey(Tab, on_delete=models.CASCADE)
     type_pk = models.ForeignKey(Type, on_delete=models.DO_NOTHING)
+    note_index = models.IntegerField(default=0)
     x = models.IntegerField()
     y = models.IntegerField()
     z = models.IntegerField()
     width = models.IntegerField()
     height = models.IntegerField()
     content = models.TextField()
-    lock = models.BooleanField()
+    lock = models.BooleanField(default=False)
 
 class History(models.Model):
     user_pk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
-    tab_pk = models.ForeignKey(Tab, on_delete=models.CASCADE)
+    board_pk = models.ForeignKey(Board, on_delete=models.DO_NOTHING)
+    tab_pk = models.ForeignKey(Tab, on_delete=models.DO_NOTHING)
     note_pk = models.ForeignKey(Note, on_delete=models.DO_NOTHING)
     type_pk = models.ForeignKey(Type, on_delete=models.DO_NOTHING)
     x = models.IntegerField()
@@ -43,3 +48,4 @@ class History(models.Model):
     height = models.IntegerField()
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    activate = models.BooleanField(default=True)
