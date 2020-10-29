0f2a83e5640350b0eafb8a15b0fc6d26c0514500
JANG JONG HA <jangjongha.sw@gmail.com>
2020-10-30 00:37:00 +0900

    [Backend] Add super_admin verification for Board Put & Delete request
    이제 보드의 수정 및 삭제에 대해 슈퍼 관리자 여부를 확인합니다.
    
    Signed-off-by: JANG JONG HA <jangjongha.sw@gmail.com>

--- Backend/board/views.py
+++ Backend/board/views.py
@@ -88,6 +88,37 @@ class BoardDetailView(mixins.RetrieveModelMixin,
         serializer = BoardViewSerializer(instance)
         return Response(serializer.data)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        if request.user != instance.super_admin:
+            return Response({
+                "status": status.HTTP_401_UNAUTHORIZED
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        resp = BoardViewSerializer(instance)
+        return Response(resp.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user != instance.super_admin:
+            return Response({
+                "status": status.HTTP_401_UNAUTHORIZED
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     def get(self, request, *args, **kwargs):
         if not request.user:
             return Response({
