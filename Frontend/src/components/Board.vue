1fc43ac3803818cf312f1d39604e8df7e888fbb4
김시영 <ssks1013@gmail.com>
2020-10-30 20:32:09 +0900

    note index 삭제

--- Frontend/src/components/Board.vue
+++ Frontend/src/components/Board.vue
@@ -35,7 +35,7 @@
         <svg @mousedown="activatedNote=index" @mouseup="patchNote(note.note_index)" class="line" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="40" height="40" viewBox="0 0 24 24"><path d="M16,12V4H17V2H7V4H8V12L6,14V16H11.2V22H12.8V16H18V14L16,12Z" /></svg>
         <div class="content" v-html="note.content">
         </div>
-        {{ note.note_index }}
+        <!-- {{ note.note_index }} -->
         <div style="position:absolute;left:5px;bottom:5px;">
             <v-icon class="del_btn" @click="delNote(note.note_index)">mdi-trash-can-outline</v-icon>
         </div>
