853d2cb4f7beafcf73ba1c26acd73444538ac208
JANG JONG HA <jangjongha.sw@gmail.com>
2020-11-02 23:47:26 +0900

    [Backend] Image/File upload system updated
    이제 이미지 파일은 확장자와 함께 직접 접근이 가능합니다. 그 외의 파일은 암호화 키로 접근합니다.
    
    Signed-off-by: JANG JONG HA <jangjongha.sw@gmail.com>

--- Backend/file/models.py
+++ Backend/file/models.py
@@ -2,12 +2,18 @@ import os
 from uuid import uuid4
 from datetime import datetime
 from django.db import models
+from django.conf import settings
 
 def get_file_path(instance, filename):
-    # ext = filename.split('.')[-1]
+    ext = filename.split('.')[-1]
     ymd_path = datetime.now().strftime('%Y/%m/%d')
-    uuid_name = "{}".format(uuid4().hex)
-    return '/'.join(['files/', ymd_path, uuid_name])
+    if ext in settings.IMAGE_TYPE:
+        uuid_name = "{}.{}".format(uuid4().hex, ext)
+        return '/'.join(['images/', uuid_name])
+    else:
+        uuid_name = "{}".format(uuid4().hex)
+        return '/'.join(['files/', ymd_path, uuid_name])
+    
 
 class File(models.Model):
     file = models.FileField(upload_to=get_file_path, blank=False, null=True)
--- Backend/file/views.py
+++ Backend/file/views.py
@@ -16,16 +16,25 @@ class FileUploadView(APIView):
     parser_class = (FileUploadParser,)
 
     def post(self, request, *args, **kwargs):
+        # 이 케이스는 이미지 파일 이외의 것을 의미한다.
         file_serializer = FileSerializer(data=request.data)
         if file_serializer.is_valid():
-            # if request.FILES:
-            #     if 'file' in request.FILES.keys():
-            #         file_serializer.filename = request.FILES['file'].name
             model_obj = file_serializer.save()
             model_obj.filename = request.FILES['file'].name
             model_obj.hex_name = str(model_obj).split("/")[-1]
             model_obj.save()
-            resp = file_serializer.data
+            result = file_serializer.data
+
+            if request.FILES['file'].name.split(".")[-1] in settings.IMAGE_TYPE:
+                file_url = settings.BACKEND_URL + "api/v1/" + str(model_obj)
+            else:
+                file_url = settings.BACKEND_URL + "api/v1/file/download/" + "/".join(str(model_obj).split("/")[4:])
+
+            resp = {
+                'file_url': file_url,
+                'original_filename': result["filename"],
+            }
+
             return Response(resp, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -43,4 +52,4 @@ class FileDownloadView(APIView):
                 response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_url)[0])
                 response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'%s' % quote_file_url
                 return response
-            raise Http404
+        raise Http404
