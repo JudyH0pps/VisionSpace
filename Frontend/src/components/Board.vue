2bb8633c733c0d79dede23523d33aa825f268895
김시영 <ssks1013@gmail.com>
2020-11-12 11:25:25 +0900

    디자인확인

--- Frontend/src/components/Board.vue
+++ Frontend/src/components/Board.vue
@@ -84,7 +84,7 @@
             d="M16,12V4H17V2H7V4H8V12L6,14V16H11.2V22H12.8V16H18V14L16,12Z"
           />
         </svg>
-        {{ note }}
+        <!-- {{ note }} -->
         <div
           class="content"
           @mouseover="isZend = note.note_index"
